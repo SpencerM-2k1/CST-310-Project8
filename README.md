16b2d5d22b21d0136ea6cb0b18b78d686a30e5a6
Spencer Meren <spencer.meren@gmail.com>
2024-03-30 23:31:15 -0700

    Added README.md, zoom function

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project 8: Simple Demo Scene
+# Written by Spencer Meren and Samson Becenti
+
+## Build Project
+
+Compile SolarSystem.cpp:
+
+	g++ SolarSystem.cpp -o main -lglfw -lGLU -lGL -lglut
+
+## Execute
+
+Run SolarSystem.cpp:
+
+	./main
+
+
--- SolarSystem.cpp
+++ SolarSystem.cpp
@@ -18,7 +18,7 @@
 #include "Planet.h"
 
 // GLUT escape key id for readability
-#define KEY_ESCAPE 2
+#define KEY_ESCAPE 27
 
 //Planets (declared as global due to init function)
 Planet sun(2, 56, 24);
@@ -44,64 +44,10 @@ Planet uranus(0.65, 25, 30);
 Planet neptune(0.60, 20, 30); 
 
 
-/* = TODO =
-* Add keyboard control (r) to rotate the whole image.
-* With keyboard, add zoom in/zoom out (keys +: Zoom in, -: Zoom out).
-* Add more cubes with different colors, illumination, and brightness.
-* Add two vertical planes, one on the left and one on the right, and make the cubes bounce.
-*/
-
-// The cube has opposite corners at (0,0,0) and (1,1,1), which are black and
-// white respectively.  The x-axis is the red gradient, the y-axis is the
-// green gradient, and the z-axis is the blue gradient.  The cube's position
-// and colors are fixed.
-// namespace Cube {
-
-//   const int NUM_VERTICES = 8;
-//   const int NUM_FACES = 6;
-
-//   GLint vertices[NUM_VERTICES][3] = {
-//     {0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
-//     {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
-
-//   GLint faces[NUM_FACES][4] = {
-//     {1, 5, 7, 3}, {5, 4, 6, 7}, {4, 0, 2, 6},
-//     {3, 7, 6, 2}, {0, 1, 3, 2}, {0, 4, 5, 1}};
-
-//   GLfloat vertexColors[NUM_VERTICES][3] = {
-//     {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {0.0, 1.0, 1.0},
-//     {1.0, 0.0, 0.0}, {1.0, 0.0, 1.0}, {1.0, 1.0, 0.0}, {1.0, 1.0, 1.0}};
-
-//   void draw() {
-//     glBegin(GL_QUADS);
-//     for (int i = 0; i < NUM_FACES; i++) {
-//       for (int j = 0; j < 4; j++) {
-//         glColor3fv((GLfloat*)&vertexColors[faces[i][j]]);
-//         glVertex3iv((GLint*)&vertices[faces[i][j]]);
-//       }
-//     }
-//     glEnd();
-//   }
-// }
-
-
 //Player-controlled variables
 
-//Figure movement
-GLboolean stopFigure = false;
-
-//Rotation control
-GLfloat pitch = 0.0f,
-        yaw =   0.0f,
-        roll =  0.0f;
-
-//Offset control
-GLfloat figureX = 0.0f,
-        figureY = 0.0f,
-        figureZ = 0.0f;
-
 //Zoom control
-GLfloat scale = 1.0f;
+GLint distance = 28;
 
 // Display and Animation. To draw we just clear the window and draw the cube.
 // Because our main window is double buffered we have to swap the buffers to
@@ -143,6 +89,9 @@ void drawStars() {
 void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
+  glLoadIdentity();
+  gluLookAt(0, distance, distance, 0, 0, 0, 0, -1, 0);
+
   mercury.orbitStep();
   venus.orbitStep();
   
@@ -199,9 +148,7 @@ void timer(int v) {
   static GLfloat u = 0.0;
   // if(!stopFigure)
   //   u += 0.01;
-  glLoadIdentity();
   //gluLookAt(8*cos(u), 7*cos(u)-1, 4*cos(u/3)+2, .5, .5, .5, cos(u), 1, 0);
-  gluLookAt(0, 14, 14, 0, 0, 0, 0, -1, 0);
   glutPostRedisplay();
   glutTimerFunc(1000/60.0, timer, v);
 }
@@ -214,7 +161,7 @@ void reshape(int w, int h) {
   glViewport(0, 0, w, h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(60.0, GLfloat(w) / GLfloat(h), 0.5, 40.0);
+  gluPerspective(60.0, GLfloat(w) / GLfloat(h), 0.5, 120.0);
   glMatrixMode(GL_MODELVIEW);
 }
 
@@ -295,19 +242,17 @@ void key_callback(unsigned char key, int dummy1, int dummy2)
   {
     case (KEY_ESCAPE): //Quit
       exit(0);
-    case ('s'):
-      stopFigure = true;
+    case ('+'):
+    case ('='):
+    distance--;
+    if (distance < 4)
+      distance = 4;
       break;
-    case ('c'):
-      stopFigure = false;
-      break;
-    case ('u'): //Move up
-      if (stopFigure)
-        figureY += 0.1f;
-      break;
-    case ('d'): //Move down
-      if (stopFigure)
-        figureY -= 0.1f;
+    case ('-'):
+    case ('_'):
+    distance++;
+    if (distance > 35)
+      distance = 35;
       break;
   }
 }
@@ -316,7 +261,7 @@ void key_callback(unsigned char key, int dummy1, int dummy2)
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(500, 500);
+  glutInitWindowSize(750, 750);
   glutCreateWindow("The RGB Color Cube");
   glutReshapeFunc(reshape);
   glutTimerFunc(100, timer, 0);
